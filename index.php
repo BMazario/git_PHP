010d6333efa8bac001b3a7b25a0b01041945d064
Brenda Mazario <brendammadureira@gmail.com>
2026-05-06 10:39:11 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$n1 = readline("Digite a  1° nota: ");
+$n2 = readline("Digite a  2° nota: ");
+$n3 = readline("Digite a  3° nota: ");
+$n4 = readline("Digite a  4g nota: ");
+
+$media = ($n1 + $n2 + $n3 + $n4) / 4;
+
+echo "A média das notas é: $media";
+
+
+?>
